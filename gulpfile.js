0de282e4fc97fbc3dbb225ff69529c4e421d9b3e
Your Name <you@example.com>
2016-05-28 19:06:13 +0700

    v3

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
+
+
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
@@ -15,9 +19,21 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
+    .pipe(browserSync.stream())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(['scss/**/*.scss'], ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./css/app.css").on('change', browserSync.reload);
+  
 });
+
+
